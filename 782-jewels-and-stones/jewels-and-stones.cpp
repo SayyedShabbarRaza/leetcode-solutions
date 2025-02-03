0995a6277a818849d4c5c7d4c927869e9e0dc3da
Sayyed Shabbar Raza <154327758+SayyedShabbarRaza@users.noreply.github.com>
2025-02-03 09:18:10 +0500

    Time: 0 ms (100.00%) | Memory: 9.1 MB (7.78%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -3,14 +3,16 @@ public:
     int numJewelsInStones(string jewels, string stones) {
         int jn=jewels.size();
         int sn=stones.size();
-        int count=0;
-        for(int i=0;i<jn;i++){
-            for(int j=0;j<sn;j++){
-                if(jewels[i]==stones[j]){
-                    count++;
-                }
-            }
+        unordered_multiset<char>umstones(stones.begin(),stones.end());
+        unordered_multiset<char>umjewels(jewels.begin(),jewels.end());
+        int num=0;
+        for(char chr:umstones){
+            if(umjewels.find(chr)==umjewels.end()){
+
+            }else{
+                num++;
+            }            
         }
-        return count;
+        return num;
     }
 };
