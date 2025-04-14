c575e67aaa11ac9f72f441886117b712a2707d71
Sayyed Shabbar Raza <154327758+SayyedShabbarRaza@users.noreply.github.com>
2025-04-14 16:06:02 +0500

    Time: 3 ms (37.72%) | Memory: 61.1 MB (25.04%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int left = 0;      // start of window
-        int usedBits = 0;  // keeps track of all 1s in current window
-        int maxLength = 0; // final answer
+        int left = 0;     
+        int usedBits = 0;  
+        int maxLength = 0;
 
         for (int right = 0; right < nums.size(); ++right) {
-            // If nums[right] shares any bit with usedBits, shrink from left
             while ((usedBits & nums[right]) != 0) {
-                usedBits ^= nums[left]; // remove nums[left] from usedBits
-                left++;                 // move window start forward
+                usedBits ^= nums[left]; 
+                left++;              
             }
-
-            usedBits |= nums[right]; // include nums[right] in window
-            maxLength = max(maxLength, right - left + 1); // update max length
+            usedBits |= nums[right]; 
+            maxLength = max(maxLength, right - left + 1); 
         }
-
         return maxLength;
     }
 };
