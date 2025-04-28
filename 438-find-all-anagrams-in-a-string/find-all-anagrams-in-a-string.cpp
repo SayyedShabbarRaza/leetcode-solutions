a7bab8fb8b06eba8844f1b4e68bccd4bf670525f
Sayyed Shabbar Raza <154327758+SayyedShabbarRaza@users.noreply.github.com>
2025-04-28 12:23:50 +0500

    Time: 4 ms (70.40%) | Memory: 11.7 MB (42.38%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        unordered_map<char, int> umap;
+        vector<int> ans;
+        int l = 0, r = 0;
+        int needed = p.size();
+        
+        for (char c : p) {
+            umap[c]++;
+        }
+
+
+        while (r < s.size()) {
+            // If s[r] is needed, reduce needed count
+            if (umap[s[r]] > 0) {
+                needed--;
+            }
+            umap[s[r]]--;
+            
+            // Now window size = r - l + 1
+            if (r - l + 1 == p.size()) {
+                if (needed == 0) {
+                    ans.push_back(l);
+                }
+                // Before sliding the window, restore the effect of s[l]
+                if (umap[s[l]] >= 0) {
+                    needed++;
+                }
+                umap[s[l]]++;
+                l++; // Move left pointer
+            }
+            r++; // Expand right pointer
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
+// class Solution {
+// public:
+// //Close to solution after 37 min but can't find the right way(avoid duplicates)
+//     vector<int> findAnagrams(string s, string p) {
+//         unordered_map<char,int>umap;
+//         int r=0,l=0;
+//         vector<int>ans;
+//         int length=0;
+//         for(char c:p){
+//             umap[c]++;
+//         }
+
+//         while(r<s.size()){
+            
+            
+//             if(umap.find(s[r])==umap.end()){
+//                 l=r;
+//                 length=0;
+//             }
+//             length=max(length,r-l+1);
+//             if(length==p.size()){
+//                 ans.push_back(l);
+//             }
+//             r++;
+//         }
+//         return ans;
+
+//     }
+// };
