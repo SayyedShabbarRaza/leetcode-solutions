1007ca1829df341db06afb8fcd7456b5c967e8fd
Sayyed Shabbar Raza <154327758+SayyedShabbarRaza@users.noreply.github.com>
2025-04-29 07:04:21 +0500

    Time: 35 ms (14.74%) | Memory: 36.7 MB (8.32%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
+    bool allZeros(vector<int>alpha){
+        bool isZero=false;
+        for(int i=0;i<alpha.size();i++){
+            if(alpha[i]!=0){
+                isZero=true;
+            }
+        }
+        return isZero?false:true;
+    }
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char, int> umap;
-        vector<int> ans;
-        int l = 0, r = 0;
-        int needed = p.size();
-        
-        for (char c : p) {
-            umap[c]++;
+        vector<int>alpha(26,0);
+        int l=0,r=0,n=s.size();
+        vector<int>ans;
+
+        for(char c:p){
+            alpha[c-'a']++;
         }
 
+        while(r<n){
+            alpha[s[r]-'a']--;
 
-        while (r < s.size()) {
-            // If s[r] is needed, reduce needed count
-            if (umap[s[r]] > 0) {
-                needed--;
+            if(r-l+1==p.size()){
+            if(allZeros(alpha)){
+                ans.push_back(l);
             }
-            umap[s[r]]--;
-            
-            // Now window size = r - l + 1
-            if (r - l + 1 == p.size()) {
-                if (needed == 0) {
-                    ans.push_back(l);
-                }
-                // Before sliding the window, restore the effect of s[l]
-                if (umap[s[l]] >= 0) {
-                    needed++;
-                }
-                umap[s[l]]++;
-                l++; // Move left pointer
+            alpha[s[l]-'a']++;
+                l++;
+
             }
-            r++; // Expand right pointer
+            r++;
         }
-
         return ans;
     }
 };
@@ -69,5 +67,46 @@ public:
 //         }
 //         return ans;
 
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+//     vector<int> findAnagrams(string s, string p) {
+//         unordered_map<char, int> umap;
+//         vector<int> ans;
+//         int l = 0, r = 0;
+//         int needed = p.size();
+        
+//         for (char c : p) {
+//             umap[c]++;
+//         }
+
+
+//         while (r < s.size()) {
+//             // If s[r] is needed, reduce needed count
+//             if (umap[s[r]] > 0) {
+//                 needed--;
+//             }
+//             umap[s[r]]--;
+            
+//             // Now window size = r - l + 1
+//             if (r - l + 1 == p.size()) {
+//                 if (needed == 0) {
+//                     ans.push_back(l);
+//                 }
+//                 // Before sliding the window, restore the effect of s[l]
+//                 if (umap[s[l]] >= 0) {
+//                     needed++;
+//                 }
+//                 umap[s[l]]++;
+//                 l++; // Move left pointer
+//             }
+//             r++; // Expand right pointer
+//         }
+
+//         return ans;
 //     }
 // };
