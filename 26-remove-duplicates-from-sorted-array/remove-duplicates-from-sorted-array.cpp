1dc8bee79b9e23036953eb985a7897c26b6ac8c4
Sayyed Shabbar Raza <154327758+SayyedShabbarRaza@users.noreply.github.com>
2025-05-08 07:20:46 +0500

    Time: 0 ms (100.00%) | Memory: 22.5 MB (96.66%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_set<int>uset;
-        int n=nums.size();
-        int i=0;
-        while(i<nums.size()){
-            if(!uset.count(nums[i])){
-                uset.insert(nums[i]);
+        unordered_set<int> uset;
+        int n = nums.size();
+        int j = 1;
+        int i = 0;
+        while (j < n) {
+            if (nums[i] != nums[j]) {
                 i++;
-            }else{
-                nums.erase(nums.begin()+i);
+                nums[i] = nums[j];
             }
+            j++;
         }
-        return nums.size();
+        return i+1;
     }
 };
 
-
-
-        // for(int i=0;i<n;i++){
-        //     if(!uset.count(nums[i])){
-        //         uset.insert(nums[i]);
-        //     }else{
-        //         nums.erase(nums.begin()+i);
-        //     }
-        // }
+// for(int i=0;i<n;i++){
+//     if(!uset.count(nums[i])){
+//         uset.insert(nums[i]);
+//     }else{
+//         nums.erase(nums.begin()+i);
+//     }
+// }
