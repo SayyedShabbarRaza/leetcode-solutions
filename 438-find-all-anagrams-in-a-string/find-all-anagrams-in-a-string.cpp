768c8a0a3ff632182a0743bb7c40d88038bac739
Sayyed Shabbar Raza <154327758+SayyedShabbarRaza@users.noreply.github.com>
2025-06-25 21:25:46 +0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (64.66%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,112 +1,49 @@
 class Solution {
 public:
-    bool allZeros(vector<int>alpha){
-        bool isZero=false;
-        for(int i=0;i<alpha.size();i++){
-            if(alpha[i]!=0){
-                isZero=true;
-            }
-        }
-        return isZero?false:true;
-    }
     vector<int> findAnagrams(string s, string p) {
-        vector<int>alpha(26,0);
-        int l=0,r=0,n=s.size();
+        int n=s.size(),i=0,j=0,size=0,m=p.size(),masla=0;
         vector<int>ans;
+        unordered_map<char,int>umap;
 
         for(char c:p){
-            alpha[c-'a']++;
+            umap[c]++;
+        }
+        while(j<n){
+        if(umap.find(s[j])!=umap.end()&&masla<m){
+            if(umap[s[j]]>0)masla++;
+            umap[s[j]]--;
+        }
+        else{
+            if(umap.find(s[i])!=umap.end()){
+            umap[s[i]]++;
+            if(umap[s[i]]>0)masla--;    //aggar condition use na ho to means aik window mein duplicate values ny hi required jagha puri kr di or kaha sab letters aa gaye hain 
+            }                
+            i++;
         }
 
-        while(r<n){
-            alpha[s[r]-'a']--;
+            size=j-i+1;
+            if(size==m){ //Window mein sab elements aa gaye if masla==m
+                if(masla==m){
+                ans.push_back(i);
+                if(umap.find(s[i])!=umap.end()){
+                umap[s[i]]++;
+                masla--;
+                // j=i;..//x               count--;
+                }
+                i++;
+                }else{
+                    if(umap.find(s[i])!=umap.end()){
+                    umap[s[i]]++;
+                    if(umap[s[i]]>0)masla--;
+                    }
+                    i++;
+                }
 
-            if(r-l+1==p.size()){
-            if(allZeros(alpha)){
-                ans.push_back(l);
             }
-            alpha[s[l]-'a']++;
-                l++;
+        
+        j++;
 
-            }
-            r++;
         }
-        return ans;
+    return ans;
     }
 };
-
-
-
-
-
-
-// class Solution {
-// public:
-// //Close to solution after 37 min but can't find the right way(avoid duplicates)
-//     vector<int> findAnagrams(string s, string p) {
-//         unordered_map<char,int>umap;
-//         int r=0,l=0;
-//         vector<int>ans;
-//         int length=0;
-//         for(char c:p){
-//             umap[c]++;
-//         }
-
-//         while(r<s.size()){
-            
-            
-//             if(umap.find(s[r])==umap.end()){
-//                 l=r;
-//                 length=0;
-//             }
-//             length=max(length,r-l+1);
-//             if(length==p.size()){
-//                 ans.push_back(l);
-//             }
-//             r++;
-//         }
-//         return ans;
-
-//     }
-// };
-
-
-
-// class Solution {
-// public:
-//     vector<int> findAnagrams(string s, string p) {
-//         unordered_map<char, int> umap;
-//         vector<int> ans;
-//         int l = 0, r = 0;
-//         int needed = p.size();
-        
-//         for (char c : p) {
-//             umap[c]++;
-//         }
-
-
-//         while (r < s.size()) {
-//             // If s[r] is needed, reduce needed count
-//             if (umap[s[r]] > 0) {
-//                 needed--;
-//             }
-//             umap[s[r]]--;
-            
-//             // Now window size = r - l + 1
-//             if (r - l + 1 == p.size()) {
-//                 if (needed == 0) {
-//                     ans.push_back(l);
-//                 }
-//                 // Before sliding the window, restore the effect of s[l]
-//                 if (umap[s[l]] >= 0) {
-//                     needed++;
-//                 }
-//                 umap[s[l]]++;
-//                 l++; // Move left pointer
-//             }
-//             r++; // Expand right pointer
-//         }
-
-//         return ans;
-//     }
-// };
